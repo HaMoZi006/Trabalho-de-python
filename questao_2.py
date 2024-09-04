a9ac52e55d0fccaff361b8407f8bbd45fbe4f350
HaMoZi006 <daorapomba@gmail.com>
2024-09-04 17:43:47 -0300

    Add files via upload

---
+++ questao_2.py
@@ -0,0 +1,14 @@
+########################################
+##ANA LUISA RIGOTTI LEITE RA: 22400558##
+########################################
+
+########################################
+##Felipe Rios dos Santos RA: 22403886###
+########################################
+
+turma = ('andré', 'fernanda', 'luiz')
+
+nome = input("\nDigite um noem para achar na lista: ").lower()
+
+if nome in turma: 
+    print(f"\n{nome} está presente na turma\n")

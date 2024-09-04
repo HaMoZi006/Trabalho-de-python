a9eebed104556244e97fe2ffc76520bae5bd1255
HaMoZi006 <daorapomba@gmail.com>
2024-09-04 17:43:20 -0300

    Add files via upload

---
+++ questao_3.py
@@ -0,0 +1,18 @@
+########################################
+##ANA LUISA RIGOTTI LEITE RA: 22400558##
+########################################
+
+########################################
+##Felipe Rios dos Santos RA: 22403886###
+########################################
+
+num_1 = 0
+num_2 = 1
+resultado = 0
+
+for i in range (0,10):
+    resultado = num_1 + num_2
+    print(resultado)
+    
+    num_2 = num_1
+    num_1 = resultado
